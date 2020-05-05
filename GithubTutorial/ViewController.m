5785ad15dff9d862d9f8b178d250947528f3f052
Robert Li <robert@roberts-macbook-pro.local>
2020-05-06 06:38:49 +0800

    revert

--- GithubTutorial/ViewController.m
+++ GithubTutorial/ViewController.m
@@ -17,10 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"viewdidload");
-    NSLog(@"github tutorial");
-    NSLog(@"another line");
-    
+    NSLog(@"viewDidLoad");
 }
 
 
